11db904e732c94ce2067a1194423e9e334d29e2c
pol <darthinsinuate@gmail.com>
2013-07-25 18:55:28 +0100

    More optimal version of FibonacciSequencev2

--- Numbers/FibonacciSequencev2.lua
+++ Numbers/FibonacciSequencev2.lua
@@ -1,18 +1,19 @@
 --
 -- This version prints the entire sequence up to N.
 
-function fib(n)
-	if n < 2 then
-		-- print(n)
-		return n
-	else
-		return fib(n - 1) + fib(n - 2)
-	end
-end
-
 count = 0
+lastNumberA = 0;
+lastNumberB = 0;
 
 while (count ~= arg[1] - 1) do
-	print(fib(count))
+	if count < 2 then
+		fibNum = count
+	else
+		fibNum = lastNumberA + lastNumberB
+	end
+
+	lastNumberA = lastNumberB
+	lastNumberB = fibNum
+	print(fibNum)
 	count = count + 1
 end
