56ea8862d0882a3e92fe16d504738fe18060c91f
pol <darthinsinuate@gmail.com>
2013-08-10 10:09:06 +0100

    Completed ReverseString program

---
+++ Text/ReverseString.py
@@ -0,0 +1,8 @@
+# **Reverse a String**
+# 
+# Enter a string and the program will reverse it and print it out.
+
+string = input("Enter string to reverse: ")
+print(string[::-1]) # ridiculously short extended slice syntax 
+
+# print(''.join(reversed(string)))
